d5956e7dbec61a32e3b16952b557dd7d2a6f93c5
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-06 20:16:11 +0300

    Create Navi component

---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {Container, Menu } from "semantic-ui-react";
+import SignedOut from "./SignedOut";
+import SignIn from "./SignIn";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  let history = useHistory();
+  function handleSignOut() {
+    setIsAuthenticated(false);
+    history.push("/");
+  }
+  function handleSignIn() {
+    setIsAuthenticated(true);
+  }
+  return (
+    <div>
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item name="HRMS" />
+          <Menu.Item name="Main Page" />
+          <Menu.Menu position="right">
+            {isAuthenticated ? (
+              <SignIn signOut={handleSignOut} />
+            ) : (
+              <SignedOut signIn={handleSignIn} />
+            )}
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
