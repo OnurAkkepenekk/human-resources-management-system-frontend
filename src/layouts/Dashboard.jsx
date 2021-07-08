3cfb3c166c804b0bdc3b7b82c6e9514a9642e4d8
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-08 20:50:53 +0300

    Update dashboard

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import Categories from "./Categories";
+import Sidebar from "./Sidebar";
 import { Grid } from "semantic-ui-react";
 import EmployerList from "../pages/EmployerList";
 import JobAdvertisement from "../pages/JobAdvertisement";
+import Candidate from "../pages/Candidate";
+import Employee from "../pages/Employee";
+import JobTitle from "../pages/JobTitle";
+
 export default function Dashboard() {
   return (
     <div>
       <Grid>
         <Grid.Row>
           <Grid.Column width={4}>
-            <Categories></Categories>
+            <Sidebar></Sidebar>
           </Grid.Column>
           <Grid.Column width={12}>
             <EmployerList></EmployerList>
             <JobAdvertisement></JobAdvertisement>
+            <Candidate></Candidate>
+            <Employee></Employee>
+            <JobTitle></JobTitle>
           </Grid.Column>
         </Grid.Row>
       </Grid>
