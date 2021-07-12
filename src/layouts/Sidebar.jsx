e1263ec155a748a0c0a4c87872b253a47611cd83
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-12 17:05:53 +0300

    Update side bar

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import City from "../components/SideBarComponents/FilterSection/City";
+import JobTitle from "../components/SideBarComponents/FilterSection/JobTitle"
+import JobAdvertisementFilter from "../components/SideBarComponents/FilterSection/JobAdvertisementFilter"
 export default function Sidebar() {
   return (
     <div>
-      <Menu pointing vertical>
-        <Menu.Item name="home" />
-        <Menu.Item name="messages" />
-        <Menu.Item name="friends" />
+      <Menu vertical>
+        <Menu.Item className="filter" >
+          <City></City>
+        </Menu.Item>
+        <Menu.Item className="filter">
+          <JobTitle></JobTitle>
+        </Menu.Item>
+        <Menu.Item className="filter">
+          <JobAdvertisementFilter/>
+        </Menu.Item>
       </Menu>
     </div>
   );
