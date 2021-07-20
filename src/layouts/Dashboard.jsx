50212b8c0f56d49c0c1454e7b1c2be5344ba0a12
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-20 22:14:05 +0300

    Add route to requests

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -5,7 +5,8 @@ import EmployerList from "../pages/EmployerList";
 import JobAdvertisement from "../pages/JobAdvertisement";
 import Candidate from "../pages/Candidate";
 import Employee from "../pages/Employee";
-
+import { Route } from "react-router";
+import JobAdvertisementAdd from "../pages/jobAdvertisement/JobAdvertisementAdd";
 export default function Dashboard() {
   return (
     <div>
@@ -15,10 +16,12 @@ export default function Dashboard() {
             <Sidebar></Sidebar>
           </Grid.Column>
           <Grid.Column width={12}>
-            <EmployerList></EmployerList>
-            <JobAdvertisement></JobAdvertisement>
+            <Route exact path="/" component={JobAdvertisement}></Route>
+            <Route exact path="/jobadvertisement" component={JobAdvertisement}></Route>
+            <Route exact path="/jobadvertisement/add" component={JobAdvertisementAdd}></Route>
+            {/* <EmployerList></EmployerList>
             <Candidate></Candidate>
-            <Employee></Employee>
+            <Employee></Employee> */}
           </Grid.Column>
         </Grid.Row>
       </Grid>
