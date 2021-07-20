b05a671e24ad9ea1b6e336cde6813f4240c15061
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-20 22:41:01 +0300

    Bugfix

---
+++ src/pages/jobAdvertisement/JobAdvertisementDetails.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+export default function JobAdvertisementDetails() {
+    const [advertisementDetail, setAdvertisementDetail] = useState({})
+  return <div></div>;
+}
