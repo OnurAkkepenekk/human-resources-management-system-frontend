73cca119cde49e23b067b2c8391256b3e17d490f
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-08-07 13:47:15 +0300

    Create job advertisement details page

--- src/pages/jobAdvertisement/JobAdvertisementDetails.jsx
+++ src/pages/jobAdvertisement/JobAdvertisementDetails.jsx
@@ -1,5 +1,20 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import JobAdvertisementService from "../../services/jobAdvertisement";
 export default function JobAdvertisementDetails() {
-    const [advertisementDetail, setAdvertisementDetail] = useState({})
-  return <div></div>;
+  //parametreleri obje olarak verir
+  let { id } = useParams();
+  const [advertisementDetail, setAdvertisementDetail] = useState({});
+
+  useEffect(() => {
+    let jobAdvertisementService = new JobAdvertisementService();
+    jobAdvertisementService
+      .getJobAdvertisementById(id)
+      .then((result) => setAdvertisementDetail(result.data.data));
+  }, []);
+  return <div>{console.log(advertisementDetail)}
+  
+  
+  
+  </div>;
 }
