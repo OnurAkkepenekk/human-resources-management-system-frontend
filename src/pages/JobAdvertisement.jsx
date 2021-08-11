34584b22d94a7e0c48817561a120a6d7820ac462
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-08-11 18:23:41 +0300

    Update JobAdvertisement.jsx

--- src/pages/JobAdvertisement.jsx
+++ src/pages/JobAdvertisement.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import JobAdvertisementService from "../services/jobAdvertisement";
 import { Table } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import "../css/JobAdvertisement/JobAdvertisementList.css"
 export default function JobAdvertisement() {
   let jobAdvertisementService = new JobAdvertisementService();
 
@@ -15,16 +16,18 @@ export default function JobAdvertisement() {
 
   return (
     <div>
-      JobAdvertisement
+      <h1>JobAdvertisement</h1>
       {console.log(jobAdvertisements)}
       <Table striped>
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>Job Description</Table.HeaderCell>
-            <Table.HeaderCell>Publish Date</Table.HeaderCell>
-            <Table.HeaderCell>Minimum Salary</Table.HeaderCell>
-            <Table.HeaderCell>Maximum Salary</Table.HeaderCell>
+            <Table.HeaderCell>Company</Table.HeaderCell>
+            <Table.HeaderCell>Position</Table.HeaderCell>
+            <Table.HeaderCell>Work Time Type</Table.HeaderCell>
+            <Table.HeaderCell>Work Type</Table.HeaderCell>
             <Table.HeaderCell>Open Position Count</Table.HeaderCell>
+            <Table.HeaderCell>Publish Date</Table.HeaderCell>
             <Table.HeaderCell>Last Apply Date</Table.HeaderCell>
             <Table.HeaderCell></Table.HeaderCell>
           </Table.Row>
@@ -34,12 +37,18 @@ export default function JobAdvertisement() {
           {jobAdvertisements.map((jobAdvert) => (
             <Table.Row key={jobAdvert.id}>
               <Table.Cell>{jobAdvert.jobDescription}</Table.Cell>
-              <Table.Cell>{jobAdvert.publishDate}</Table.Cell>
-              <Table.Cell>{jobAdvert.minSalary}</Table.Cell>
-              <Table.Cell>{jobAdvert.maxSalary}</Table.Cell>
+              <Table.Cell>
+              <Link to={`/employer/${jobAdvert.employer.id}`}>{jobAdvert.employer.companyName}</Link>
+              </Table.Cell>
+              <Table.Cell>{jobAdvert.jobPosition.jobTitle}</Table.Cell>
+              <Table.Cell>{jobAdvert.workTimeType.workTimeTypeName}</Table.Cell>
+              <Table.Cell>{jobAdvert.workType.workTypeName}</Table.Cell>
               <Table.Cell>{jobAdvert.openPositionCount}</Table.Cell>
               <Table.Cell>{jobAdvert.lastApplyDate}</Table.Cell>
-              <Table.Cell><Link to={`/jobadvertisements/${jobAdvert.id}`}>Review</Link></Table.Cell>
+              <Table.Cell>{jobAdvert.publishDate}</Table.Cell>
+              <Table.Cell>
+                <Link to={`/jobadvertisements/${jobAdvert.id}`}>Review</Link>
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
