2d70be6cf6e751fc20ee09ca5e4403bc61fcdbec
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-08-11 18:23:09 +0300

    update dashboard

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,31 +1,60 @@
 import React from "react";
-import Sidebar from "./Sidebar";
-import { Grid } from "semantic-ui-react";
-import EmployerList from "../pages/EmployerList";
+import { Container } from "semantic-ui-react";
+import { Grid, Image, Rail, Segment } from "semantic-ui-react";
 import JobAdvertisement from "../pages/JobAdvertisement";
-import Candidate from "../pages/Candidate";
-import Employee from "../pages/Employee";
 import { Route } from "react-router";
 import JobAdvertisementAdd from "../pages/jobAdvertisement/JobAdvertisementAdd";
 import JobAdvertisementDetails from "../pages/jobAdvertisement/JobAdvertisementDetails";
+import "../css/main.css";
+import EmployerInfo from "../pages/employer/EmployerInfo";
+import Sidebar from "./Sidebar";
+import "../css/Dashboard/dashboard.css";
+import Leftbar from "./Leftbar";
 export default function Dashboard() {
   return (
     <div>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={4}>
-            <Sidebar></Sidebar>
-          </Grid.Column>
-          <Grid.Column width={12}>
-            <Route exact path="/" component={JobAdvertisement}></Route>
-            <Route exact path="/jobadvertisement" component={JobAdvertisement}></Route>
-            <Route exact path="/jobadvertisement/add" component={JobAdvertisementAdd}></Route>
-            <Route path="/jobadvertisements/:id" component={JobAdvertisementDetails}></Route>
-            {/* <EmployerList></EmployerList>
-            <Candidate></Candidate>
-            <Employee></Employee> */}
-          </Grid.Column>
-        </Grid.Row>
+      <Grid centered columns={2}>
+        <Grid.Column>
+          <Segment>
+            <Container className="main">
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column>
+                    <Route exact path="/" component={JobAdvertisement}></Route>
+                    <Route
+                      exact
+                      path="/jobadvertisement"
+                      component={JobAdvertisement}
+                    ></Route>
+                    <Route
+                      exact
+                      path="/jobadvertisement/add"
+                      component={JobAdvertisementAdd}
+                    ></Route>
+                    <Route
+                      path="/jobadvertisements/:id"
+                      component={JobAdvertisementDetails}
+                    ></Route>
+                    <Route
+                      path="/employer/:id"
+                      component={EmployerInfo}
+                    ></Route>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Container>
+
+            <Rail position="left">
+              <Segment><Leftbar/></Segment>
+            </Rail>
+
+            <Rail position="right">
+              <Segment>
+                <Sidebar />
+              </Segment>
+            </Rail>
+          </Segment>
+        </Grid.Column>
       </Grid>
     </div>
   );
