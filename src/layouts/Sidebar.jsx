bf5404a93a65a1e24e14948098a647ec873c6563
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-08-11 18:23:53 +0300

    update sidebar

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,22 +1,15 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
-import City from "../components/SideBarComponents/FilterSection/City";
-import JobTitle from "../components/SideBarComponents/FilterSection/JobTitle"
-import JobAdvertisementFilter from "../components/SideBarComponents/FilterSection/JobAdvertisementFilter"
+import { Icon, Input, Segment } from "semantic-ui-react";
+
 export default function Sidebar() {
   return (
-    <div>
-      <Menu vertical>
-        <Menu.Item className="filter" >
-          <City></City>
-        </Menu.Item>
-        <Menu.Item className="filter">
-          <JobTitle></JobTitle>
-        </Menu.Item>
-        <Menu.Item className="filter">
-          <JobAdvertisementFilter/>
-        </Menu.Item>
-      </Menu>
-    </div>
+    <Segment.Group horizontal>
+      <Segment>
+        <Input
+          icon={<Icon name="search" inverted circular link />}
+          placeholder="Search..."
+        />
+      </Segment>
+    </Segment.Group>
   );
 }
