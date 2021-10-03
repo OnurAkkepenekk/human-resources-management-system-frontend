e627f32d380df2f63ee183a720406082caa23391
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-03 18:04:08 +0300

    Update JobTitle.jsx

--- src/components/SideBarComponents/FilterSection/JobTitle.jsx
+++ src/components/SideBarComponents/FilterSection/JobTitle.jsx
@@ -1,26 +1,48 @@
 import React, { useState, useEffect } from "react";
 import JobTitleService from "../../../services/jobTitle";
-import { Dropdown } from "semantic-ui-react";
+import { Select } from 'antd';
 export default function Job() {
   const [jobs, setJobs] = useState([]);
   useEffect(() => {
     let jobTitleService = new JobTitleService();
     jobTitleService.getJobs().then((result) => setJobs(result.data.data));
   }, []);
+  const { Option } = Select;
 
-  const stateOptions = jobs.map((job) => ({
-    key: job.id,
-    text: job.jobTitle,
-    value: job.id,
-  }));
+  function onChange(value) {
+    console.log(`selected ${value}`);
+  }
+
+  function onBlur() {
+    console.log('blur');
+  }
+
+  function onFocus() {
+    console.log('focus');
+  }
+
+  function onSearch(val) {
+    console.log('search:', val);
+  }
   return (
     <div>
-      <Dropdown
-        placeholder="Select Job"
-        search
-        selection
-        options={stateOptions}
-      ></Dropdown>
+      <Select
+        showSearch
+        style={{ width: 200 }}
+        placeholder="Select a city"
+        optionFilterProp="children"
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onSearch={onSearch}
+        filterOption={(input, option) =>
+          option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+      >
+        {jobs.map(job => (
+          <Option key={job.id} value={job.id}>{job.jobTitle}</Option>
+        ))}
+      </Select>,
     </div>
   );
 }
