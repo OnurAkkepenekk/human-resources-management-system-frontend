1e9d17c0c16229fd55a66bbcffff7f1cb5d55d0e
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-03 18:03:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
+import "antd/dist/antd.css";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <BrowserRouter>
