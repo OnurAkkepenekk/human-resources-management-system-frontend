d32a18c89b70a092a150a30fbe070cee4b5a032a
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-03 18:02:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Dashboard from "./layouts/Dashboard";
 import "semantic-ui-css/semantic.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/main.css";
+import 'antd/dist/antd.css'
 function App() {
   return (
-    <div className="App main-background">
-      <Navi></Navi>
+    <div className="App">
       <Dashboard />
     </div>
   );
