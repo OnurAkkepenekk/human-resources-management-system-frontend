d67a1abb35908b1414fa03f9630fe22113701370
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-03 18:04:02 +0300

    Update City.jsx

--- src/components/SideBarComponents/FilterSection/City.jsx
+++ src/components/SideBarComponents/FilterSection/City.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import CityService from "../../../services/cityService";
-import { Dropdown } from "semantic-ui-react";
+import { Select } from 'antd';
 export default function City() {
   const [cities, setCities] = useState([]);
 
@@ -10,20 +10,42 @@ export default function City() {
     console.log(cities);
   }, []);
 
-  const stateOptions = cities.map((city) => ({
-    key: city.id,
-    text: city.cityName,
-    value: city.id,
-  }));
+  const { Option } = Select;
 
+  function onChange(value) {
+    console.log(`selected ${value}`);
+  }
+
+  function onBlur() {
+    console.log('blur');
+  }
+
+  function onFocus() {
+    console.log('focus');
+  }
+
+  function onSearch(val) {
+    console.log('search:', val);
+  }
   return (
     <div>
-      <Dropdown
-        placeholder="Select City"
-        search
-        selection
-        options={stateOptions}
-      ></Dropdown>
+      <Select
+        showSearch
+        style={{ width: 200 }}
+        placeholder="Select a city"
+        optionFilterProp="children"
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onSearch={onSearch}
+        filterOption={(input, option) =>
+          option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+      >
+        {cities.map(city => (
+          <Option key={city.id} value={city.cityId}>{city.cityName}</Option>
+        ))}
+      </Select>,
     </div>
   );
 }
