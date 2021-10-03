441248c6c7d8ddcd7114d283546440228bbe9e20
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-03 18:03:34 +0300

    Update HRMSTextInput.jsx

--- src/utilities/customFormControls/HRMSTextInput.jsx
+++ src/utilities/customFormControls/HRMSTextInput.jsx
@@ -1,19 +1,21 @@
-import { useField, Field } from "formik";
+import { useField } from "formik";
+import {
+  Form
+} from 'antd';
 import React from "react";
 import { FormField, Label } from "semantic-ui-react";
-import { Dropdown } from "semantic-ui-react";
 export default function HRMSTextInput({ ...props }) {
   const [field, meta] = useField(props);
   return (
     <div>
-      <FormField error={meta.touched && !!meta.error}>
+      <Form.Item error={meta.touched && !!meta.error}>
         <input {...field} {...props}>
           {props.children}
         </input>
         {meta.touched && !!meta.error ? (
           <Label pointing basic color="red" content={meta.error}></Label>
         ) : null}
-      </FormField>
+      </Form.Item>
     </div>
   );
 }
