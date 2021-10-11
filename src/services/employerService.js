24a475b90bb7501b150126487262fbf288d23f50
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-11 23:27:22 +0300

    add new methods to add user

--- src/services/employerService.js
+++ src/services/employerService.js
@@ -7,4 +7,8 @@ export default class EmployerService {
   getEmployer(id) {
     return axios.get("http://localhost:8080/api/employers/id?id=" + id);
   }
+
+  addEmployer(employer) {
+    return axios.post("http://localhost:8080/api/employers/add", employer);
+  }
 }
