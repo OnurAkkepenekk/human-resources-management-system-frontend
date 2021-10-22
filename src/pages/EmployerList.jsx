1bab2e2a7674defaea340ad4cc2ce89b9e64f983
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-23 00:32:26 +0300

    Update EmployerList.jsx

--- src/pages/EmployerList.jsx
+++ src/pages/EmployerList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import EmployerService from "../services/employerService";
-import { Card, Icon, Avatar, Col, Row } from "antd";
+import { Link } from "react-router-dom";
+import { Card, Spin, Space, Avatar, Col, Row } from "antd";
 const { Meta } = Card;
 export default function EmployerList() {
   const [employers, setEmployers] = useState([]);
@@ -10,26 +11,37 @@ export default function EmployerList() {
     employerService.getEmployers().then((result) => {
       setEmployers(result.data.data);
       console.log(result.data.data);
+      setLoading(true);
     });
   }, []);
 
   return (
-      <div style={{ background: "#ECECEC", padding: "30px" }}>
-        <Row gutter={16}>
-          {employers.map((employer) => (
-            <Col span={8} key={employer.id}>
-              <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
-                <Meta
-                  avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                  }
-                  title={employer.companyName}
-                  description={employer.email}
-                />
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
+    <>
+      {loading ?
+        <div style={{ background: "#ECECEC", padding: "30px" }}>
+          <Row gutter={16}>
+            {employers.map((employer) => (
+              <Col span={8} key={employer.id} onClick={() => console.log(employer)}>
+                <Card style={{ width: 300, marginTop: 16 }} loading={false}  hoverable={true}>
+                  <Link to={`/employer/${employer.id}`}>
+                    <Meta
+                      avatar={
+                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                      }
+                      title={employer.companyName}
+                      description={employer.email}
+                    />
+                  </Link>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+        :
+        <Space size="middle">
+          <Spin size="large" />
+        </Space>
+      }
+    </>
   )
 }
