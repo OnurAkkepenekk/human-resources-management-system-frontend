ce8458ff52f8da1effa3d749a463d32fbf7c7433
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-11-01 18:29:07 +0300

    bugfix

--- src/pages/cv/cvJobExperience/JobExperienceList.jsx
+++ src/pages/cv/cvJobExperience/JobExperienceList.jsx
@@ -32,51 +32,53 @@ const JobExperienceList = () => {
                 <div>
                     {
                         jobExperience.map((jobExperience) => (
-                            <Layout style={{ marginTop: 12 }} >
-                                <Header style={{ backgroundColor: '#264653', textAlign: 'right' }}>
-                                    <Button style={{ textAlign: 'right', margin: "1%" }}>Edit</Button>
-                                    <Button style={{ textAlign: 'right' }}>Delete</Button>
-                                </Header>
-                                <Content>
-                                    <Row justify="center" style={{ backgroundColor: "#E9C46A", padding: "1%" }}>
-                                        <Col span={24}>
+                            <div key={jobExperience.id}>
+                                <Layout style={{ marginTop: 12 }} >
+                                    <Header style={{ backgroundColor: '#264653', textAlign: 'right' }}>
+                                        <Button style={{ textAlign: 'right', margin: "1%" }}>Edit</Button>
+                                        <Button style={{ textAlign: 'right' }}>Delete</Button>
+                                    </Header>
+                                    <Content>
+                                        <Row justify="center" style={{ backgroundColor: "#E9C46A", padding: "1%" }}>
+                                            <Col span={24}>
 
-                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                                                <Col className="gutter-row" span={12}>
-                                                    <span style={{ textAlign: "right", marginTop: "1%", fontWeight: "bold" }}>Work Place Name :</span>
-                                                </Col>
-                                                <Col className="gutter-row" span={6}>
-                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{jobExperience.workPlaceName}</p>
-                                                </Col>
-                                            </Row>
-                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                                                <Col className="gutter-row" span={12}>
-                                                    <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Position :</span>
-                                                </Col>
-                                                <Col className="gutter-row" span={6}>
-                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{jobExperience.jobPosition}</p>
-                                                </Col>
-                                            </Row>
-                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                                                <Col className="gutter-row" span={12}>
-                                                    <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Start Date: </span>
-                                                </Col>
-                                                <Col className="gutter-row" span={6}>
-                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{jobExperience.startDate}</p>
-                                                </Col>
-                                            </Row>
-                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                                                <Col className="gutter-row" span={12}>
-                                                    <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Quit Date: </span>
-                                                </Col>
-                                                <Col className="gutter-row" span={6}>
-                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{jobExperience.quitDate}</p>
-                                                </Col>
-                                            </Row>
-                                        </Col>
-                                    </Row>
-                                </Content>
-                            </Layout>
+                                                <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                    <Col className="gutter-row" span={12}>
+                                                        <span style={{ textAlign: "right", marginTop: "1%", fontWeight: "bold" }}>Work Place Name :</span>
+                                                    </Col>
+                                                    <Col className="gutter-row" span={6}>
+                                                        <p style={{ textAlign: "left", marginTop: "1%" }}>{jobExperience.workPlaceName}</p>
+                                                    </Col>
+                                                </Row>
+                                                <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                    <Col className="gutter-row" span={12}>
+                                                        <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Position :</span>
+                                                    </Col>
+                                                    <Col className="gutter-row" span={6}>
+                                                        <p style={{ textAlign: "left", marginTop: "1%" }}>{jobExperience.jobPosition}</p>
+                                                    </Col>
+                                                </Row>
+                                                <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                    <Col className="gutter-row" span={12}>
+                                                        <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Start Date: </span>
+                                                    </Col>
+                                                    <Col className="gutter-row" span={6}>
+                                                        <p style={{ textAlign: "left", marginTop: "1%" }}>{jobExperience.startDate}</p>
+                                                    </Col>
+                                                </Row>
+                                                <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                    <Col className="gutter-row" span={12}>
+                                                        <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Quit Date: </span>
+                                                    </Col>
+                                                    <Col className="gutter-row" span={6}>
+                                                        <p style={{ textAlign: "left", marginTop: "1%" }}>{jobExperience.quitDate}</p>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                        </Row>
+                                    </Content>
+                                </Layout>
+                            </div>
                         ))
                     }
                 </div>
