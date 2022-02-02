4b1b564e8299484013fa924233275478b23b0926
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-03 00:32:29 +0300

    Update CandidateInfo.jsx

--- src/pages/candidate/CandidateInfo.jsx
+++ src/pages/candidate/CandidateInfo.jsx
@@ -1,8 +1,9 @@
-
 import { useEffect, useState } from 'react'
 import CandidateService from '../../services/candidateService';
 import { useParams } from 'react-router-dom';
-import { Row, Col, Spin, Space } from 'antd';
+import { Row, Col, Spin, Space, Divider, Layout, Button } from 'antd';
+import ImageList from '../cv/cvImage/ImageList';
+const { Header, Content } = Layout;
 
 const CandidateInfo = () => {
 
@@ -30,56 +31,76 @@ const CandidateInfo = () => {
 
 
     return (
-        <div style={{paddingTop: 10 }}>
-            {console.log(candidate)}
-            {
-                loading ?
-                    <div>
-                        <Row justify="center" gutter={16}>
-                            <Col className="gutter-row" span={12}><span style={{ textAlign: "right", marginTop: "1%", fontWeight: "bold" }}>First Name :</span></Col>
-                            <Col className="gutter-row" span={6}>
-                                <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.firstName}</p>
-                            </Col>
-                        </Row>
-                        <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                            <Col className="gutter-row" span={12}>
-                                <span style={{ textAlign: "right", marginTop: "1%", fontWeight: "bold" }}>Last Name :</span>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.lastName}</p>
-                            </Col>
-                        </Row>
-                        <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                            <Col className="gutter-row" span={12}>
-                                <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Email :</span>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.email}</p>
-                            </Col>
-                        </Row>
-                        <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                            <Col className="gutter-row" span={12}>
-                                <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Gender: </span>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.gender}</p>
-                            </Col>
-                        </Row>
-                        <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                            <Col className="gutter-row" span={12}>
-                                <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Birth Date :</span>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.birthDate}</p>
-                            </Col>
-                        </Row>
-                    </div>
-                    :
-                    <Space size="middle">
-                        <Spin size="large" />
-                    </Space>
-            }
+
+
+        <div>
+            <Divider orientation="left">Candidate Informations</Divider>
+            <Layout >
+                <Header style={{ backgroundColor: '#264653', textAlign: 'right' }}>
+                    <Button style={{ textAlign: 'right', margin: "1%" }}>Edit</Button>
+                    <Button style={{ textAlign: 'right' }}>Delete</Button>
+                </Header>
+                <Content>
+                    <Row justify="center" style={{ backgroundColor: "#E9C46A", padding: "5%" }}>
+                        <Col span={5}><ImageList /></Col>
+                        <Col span={14}>
+                            <div style={{ paddingTop: 10 }}>
+                                {console.log(candidate)}
+                                {
+                                    loading ?
+                                        <div>
+                                            <Row justify="center" gutter={16}>
+                                                <Col className="gutter-row" span={12}><span style={{ textAlign: "right", marginTop: "1%", fontWeight: "bold" }}>First Name :</span></Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.firstName}</p>
+                                                </Col>
+                                            </Row>
+                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                <Col className="gutter-row" span={12}>
+                                                    <span style={{ textAlign: "right", marginTop: "1%", fontWeight: "bold" }}>Last Name :</span>
+                                                </Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.lastName}</p>
+                                                </Col>
+                                            </Row>
+                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                <Col className="gutter-row" span={12}>
+                                                    <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Email :</span>
+                                                </Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.email}</p>
+                                                </Col>
+                                            </Row>
+                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                <Col className="gutter-row" span={12}>
+                                                    <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Gender: </span>
+                                                </Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.gender}</p>
+                                                </Col>
+                                            </Row>
+                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                <Col className="gutter-row" span={12}>
+                                                    <span style={{ textAlign: "left", marginTop: "1%", fontWeight: "bold" }}>Birth Date :</span>
+                                                </Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>{candidate.birthDate}</p>
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                        :
+                                        <Space size="middle">
+                                            <Spin size="large" />
+                                        </Space>
+                                }
+                            </div>
+                        </Col>
+                    </Row>
+                </Content>
+            </Layout>
         </div>
+
+
     )
 }
 
