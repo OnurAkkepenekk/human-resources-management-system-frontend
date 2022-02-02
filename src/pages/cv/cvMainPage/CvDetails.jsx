f1ce86e2eae66f2acf5164e1cdf8247eb50af686
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-03 00:32:12 +0300

    Update CvDetails.jsx

--- src/pages/cv/cvMainPage/CvDetails.jsx
+++ src/pages/cv/cvMainPage/CvDetails.jsx
@@ -1,4 +1,3 @@
-import { useParams } from 'react-router-dom';
 import CandidateInfo from '../../candidate/CandidateInfo';
 import EducationInformation from '../cvEducationInformation/EducationInformation';
 import JobExperienceList from '../cvJobExperience/JobExperienceList';
@@ -6,44 +5,13 @@ import SkillList from '../cvSkill/SkillList';
 import ForeignLanguageList from '../cvForeignLanguage/ForeignLanguageList';
 import LinkList from '../cvLink/LinkList';
 import CoverLetterList from '../cvCoverLetter/CoverLetterList';
-import ImageList from '../cvImage/ImageList';
-import { Row, Col, Divider, Layout, Button } from 'antd';
-const { Header, Content } = Layout;
 
 const CvDetails = () => {
     return (
         <div style={{ marginLeft: 250, marginRight: 250 }}>
-            <div>
-                <Divider orientation="left">Candidate Informations</Divider>
-                <Layout >
-                    <Header style={{ backgroundColor: '#264653', textAlign: 'right' }}>
-                        <Button style={{ textAlign: 'right', margin: "1%" }}>Edit</Button>
-                        <Button style={{ textAlign: 'right' }}>Delete</Button>
-                    </Header>
-                    <Content>
-                        <Row justify="center" style={{ backgroundColor: "#E9C46A", padding: "5%" }}>
-                            <Col span={5}><ImageList /></Col>
-                            <Col span={14}><CandidateInfo /></Col>
-                        </Row>
-                    </Content>
-                </Layout>
-            </div>
+            <CandidateInfo />
             <CoverLetterList />
-            <Divider orientation="left">Education Informations</Divider>
-            <Layout >
-                <Header style={{ backgroundColor: '#264653', textAlign: 'right' }}>
-                    <Button style={{ textAlign: 'right', margin: "1%" }}>Edit</Button>
-                    <Button style={{ textAlign: 'right' }}>Delete</Button>
-                </Header>
-                <Content>
-                    <Row justify="center" style={{ backgroundColor: "#E9C46A", padding: "1%" }}>
-                        <Col span={24}>
-                            <EducationInformation />
-                        </Col>
-                    </Row>
-                </Content>
-            </Layout>
-
+            <EducationInformation />
             <JobExperienceList />
             <SkillList />
             <ForeignLanguageList />
