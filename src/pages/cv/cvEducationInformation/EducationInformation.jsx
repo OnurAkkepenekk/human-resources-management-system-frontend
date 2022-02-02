d4b34b320cc4132c4d2cc430d8936d75e9074881
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-03 00:32:25 +0300

    Update EducationInformation.jsx

--- src/pages/cv/cvEducationInformation/EducationInformation.jsx
+++ src/pages/cv/cvEducationInformation/EducationInformation.jsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Row, Col, Spin, Space } from "antd";
+import { Row, Col, Spin, Space, Divider, Layout, Button } from "antd";
 import EducationInformationService from "../../../services/educationInformationService";
-import moment from 'moment'
+import moment from 'moment';
 
+const { Header, Content } = Layout;
 const EducationInformation = () => {
     const [educationInfo, setEducationInfo] = useState([]);
     let { candidateId } = useParams();
@@ -28,85 +29,98 @@ const EducationInformation = () => {
         <div>
             {loading ?
                 <div>
-                    {educationInfo.map((educationInfo) => (
-                        <div>
-                            <Row justify="center" gutter={16}>
-                                <Col className="gutter-row" span={12}>
-                                    <span
-                                        style={{
-                                            textAlign: "right",
-                                            marginTop: "1%",
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        University :
-                                    </span>
-                                </Col>
-                                <Col className="gutter-row" span={6}>
-                                    <p style={{ textAlign: "left", marginTop: "1%" }}>
-                                        {educationInfo.university}
-                                    </p>
-                                </Col>
-                            </Row>
+                    <Divider orientation="left">Education Informations</Divider>
+                    <Layout >
+                        <Header style={{ backgroundColor: '#264653', textAlign: 'right' }}>
+                            <Button style={{ textAlign: 'right', margin: "1%" }}>Edit</Button>
+                            <Button style={{ textAlign: 'right' }}>Delete</Button>
+                        </Header>
+                        <Content>
+                            <Row justify="center" style={{ backgroundColor: "#E9C46A", padding: "1%" }}>
+                                <Col span={24}>
+                                    {educationInfo.map((educationInfo) => (
+                                        <div>
+                                            <Row justify="center" gutter={16}>
+                                                <Col className="gutter-row" span={12}>
+                                                    <span
+                                                        style={{
+                                                            textAlign: "right",
+                                                            marginTop: "1%",
+                                                            fontWeight: "bold",
+                                                        }}
+                                                    >
+                                                        University :
+                                                    </span>
+                                                </Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>
+                                                        {educationInfo.university}
+                                                    </p>
+                                                </Col>
+                                            </Row>
 
-                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                                <Col className="gutter-row" span={12}>
-                                    <span
-                                        style={{
-                                            textAlign: "right",
-                                            marginTop: "1%",
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        Department :
-                                    </span>
-                                </Col>
-                                <Col className="gutter-row" span={6}>
-                                    <p style={{ textAlign: "left", marginTop: "1%" }}>
-                                        {educationInfo.universityDepartment}
-                                    </p>
-                                </Col>
-                            </Row>
+                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                <Col className="gutter-row" span={12}>
+                                                    <span
+                                                        style={{
+                                                            textAlign: "right",
+                                                            marginTop: "1%",
+                                                            fontWeight: "bold",
+                                                        }}
+                                                    >
+                                                        Department :
+                                                    </span>
+                                                </Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>
+                                                        {educationInfo.universityDepartment}
+                                                    </p>
+                                                </Col>
+                                            </Row>
 
-                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                                <Col className="gutter-row" span={12}>
-                                    <span
-                                        style={{
-                                            textAlign: "left",
-                                            marginTop: "1%",
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        Start Date :
-                                    </span>
-                                </Col>
-                                <Col className="gutter-row" span={6}>
-                                    <p style={{ textAlign: "left", marginTop: "1%" }}>
-                                        {moment(educationInfo.startDate).format("DD.MM.yyyy")}
-                                    </p>
+                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                <Col className="gutter-row" span={12}>
+                                                    <span
+                                                        style={{
+                                                            textAlign: "left",
+                                                            marginTop: "1%",
+                                                            fontWeight: "bold",
+                                                        }}
+                                                    >
+                                                        Start Date :
+                                                    </span>
+                                                </Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>
+                                                        {moment(educationInfo.startDate).format("DD.MM.yyyy")}
+                                                    </p>
+                                                </Col>
+                                            </Row>
+                                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
+                                                <Col className="gutter-row" span={12}>
+                                                    <span
+                                                        style={{
+                                                            textAlign: "left",
+                                                            marginTop: "1%",
+                                                            fontWeight: "bold",
+                                                        }}
+                                                    >
+                                                        Graduate Date:
+                                                    </span>
+                                                </Col>
+                                                <Col className="gutter-row" span={6}>
+                                                    <p style={{ textAlign: "left", marginTop: "1%" }}>
+                                                        {moment(educationInfo.graduateDate).format("DD.MM.yyyy")}
+                                                    </p>
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    ))
+                                    }
                                 </Col>
                             </Row>
-                            <Row justify="center" gutter={16} style={{ marginTop: "2%" }}>
-                                <Col className="gutter-row" span={12}>
-                                    <span
-                                        style={{
-                                            textAlign: "left",
-                                            marginTop: "1%",
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        Graduate Date:
-                                    </span>
-                                </Col>
-                                <Col className="gutter-row" span={6}>
-                                    <p style={{ textAlign: "left", marginTop: "1%" }}>
-                                        {moment(educationInfo.graduateDate).format("DD.MM.yyyy")}
-                                    </p>
-                                </Col>
-                            </Row>
-                        </div>
-                    ))
-                    }
+                        </Content>
+                    </Layout>
                 </div>
 
                 : (
